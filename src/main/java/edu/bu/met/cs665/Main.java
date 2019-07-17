82ae3d6c2233a618e30e08b3816bd73d8b4495af
Christopher D. Canfield <cdcanfie@bu.edu>
2019-07-17 19:18:40 -0400

    Removed example assignment 1 code.

--- src/main/java/edu/bu/met/cs665/Main.java
+++ src/main/java/edu/bu/met/cs665/Main.java
@@ -1,6 +1,5 @@
 package edu.bu.met.cs665;
 
-import edu.bu.met.cs665.example1.Person;
 import org.apache.log4j.Logger;
 
 public class Main {
@@ -14,18 +13,7 @@ public class Main {
    * @param args not used
    */
   public static void main(String[] args) {
-    Main m = new Main();
-
-    logger.info(m.doIt());
-
-
-  }
-
-
-
-  private String doIt() {
-    Person student = new Person("John", "Doe");
-    return student.getLastName() + ',' + student.getLastName();
+    logger.info("Hello");
   }
 
 }
