c10ea311cc6a97117e04f2b0e87d8e87d7651c7f
Christopher D. Canfield <cdcanfie@bu.edu>
2019-07-19 19:34:14 -0400

    Oops. Included missed files.

---
+++ src/main/java/edu/bu/met/cs665/bev/controller/BeverageController.java
@@ -0,0 +1,43 @@
+package edu.bu.met.cs665.bev.controller;
+
+/**
+ * Interface for beverage controllers, 
+ * 
+ * @author Christopher D. Canfield
+ */
+public interface BeverageController {
+  /**
+   * Beverage controller states.
+   * 
+   * @author Christopher D. Canfield
+   */
+  public enum State {
+    READY,
+    MAKING_DRINK
+  }
+  
+  /**
+   * Returns the current state of the beverage controller.
+   * @return the current state of the beverage controller.
+   */
+  public State state();
+  
+  /**
+   * Submits a drink order to the BeverageController. The controller will notify its observers of
+   * its progress using the BeverageControllerObserver callback methods.
+   * @param order the drink order to process.
+   */
+  public void submitOrder(BeverageOrder order);
+  
+  /**
+   * Adds the specified observer to this beverage controller.
+   * @param observer the observer to add.
+   */
+   public void addObserver(BeverageControllerObserver observer);
+   
+   /**
+   * Removes the specified observer from this beverage controller. If the 
+   * @param observer the observer to remove.
+   */
+   public void removeObserver(BeverageControllerObserver observer);
+}
